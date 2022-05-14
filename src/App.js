f7ac1bc12505a1f63a6185eb2123f211a2c2d5e0
Carlos Vazquez <c.vazquez1986@gmail.com>
2022-05-14 17:23:27 -0400

    added Nav and fixed others

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Nav from "./components/Nav";
+import About from "./components/About";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
+import Resume from "./components/Resume";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav />
+      <div>
+        <Routes>
+          <Route path="/" element={<About />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/resume" element={<Resume />} />
+        </Routes>
+      </div>
+      <div>
+        <Footer />
+      </div>
     </div>
   );
 }
-
 export default App;
---
+++ src/components/Nav/index.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+
+function Nav() {
+  const [links] = useState(["about", "portfolio", "contact", "resume"]);
+
+  return (
+    <header className="bg-gray-800 text-gray-300 flex flex-col justify-center md:flex-row md:justify-between pb-0 md:pb-3 content-center place-content-center">
+      <h2 className="font-hand mx-2 text-6xl self-center">Carlos</h2>
+      <nav className="">
+        <ul className="flex flex-row flex-wrap md:flex-row md:flex-nowrap self-center md:place-self-end justify-center content-center uppercase w-screen md:w-1/3">
+          {links.map((link) => (
+            <li
+              key={link}
+              className="p-2 text-center basis-1/2 md:basis-auto hover:bg-gray-200 hover:text-gray-900">
+              <NavLink
+                to={link === "about" ? "/" : link}
+                className={(navData) =>
+                  navData.isActive ? "underline underline-offset-4" : ""
+                }>
+                {link}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Nav;

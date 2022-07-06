93f22451509932d9d74be1e09a6d2faa9418a17b
Carlos Vazquez <c.vazquez1986@gmail.com>
2022-07-05 22:06:52 -0400

    edit about me

--- src/components/About.js
+++ src/components/About.js
@@ -9,8 +9,8 @@ function About() {
         className="w-1/2"
       /> 
       <p className="text-center text-lg">
-        Welcome! My name is Carlos Vazquez and I'm currently a student in the UCF Coding Boot Camp.
-        By the end of the course I will have the tools and knowledge to work on front-end and back-end jobs.
+        Welcome! My name is Carlos Vazquez and I'm a recent graduate of the UCF Coding Boot Camp.
+        I will have obtain the tools and knowledge to work on front-end and back-end jobs.
         Along with those skills I also have an art education degree, can work in a team or independently, and have a love for problem solving.  
       </p>
     </div>
